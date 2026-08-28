94f8b54fe0cd92b6c68342fae81eda37736d581f
HarshitaTripathii <harshitat216@gmail.com>
2026-08-28 23:01:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
+
+app.use("/", (req, res) => {
+  return res.json({
+    text: "hello Harshita at port 3000",
+  });
+});
